b582e293f20980975a0e20b6419f2b9526ded88f
ojiudezue <oji@udezue.com>
2026-02-28 06:49:39 +0100

    v3.6.0-c2.3: fix: initial inference on startup + version sync

--- custom_components/universal_room_automation/select.py
+++ custom_components/universal_room_automation/select.py
@@ -1,6 +1,6 @@
 """Select platform for Universal Room Automation."""
 #
-# Universal Room Automation v3.6.0-c2.2-c1
+# Universal Room Automation v3.6.0-c2.3-c2.2-c1
 # File: select.py
 # v3.6.0-c1: Added house state override and zone presence mode selects
 #
