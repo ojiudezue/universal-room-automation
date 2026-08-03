0802f12fd63d0fa4c6a2fda21a25dd6f3fbcde17
Oji Udezue <oji@outlook.com>
2026-08-03 16:48:28 -0500

    v5.50.1: Hotfix: HVAC settings options form render 500

--- custom_components/universal_room_automation/select.py
+++ custom_components/universal_room_automation/select.py
@@ -1,6 +1,6 @@
 """Select platform for Universal Room Automation."""
 #
-# Universal Room Automation vv5.50.0
+# Universal Room Automation vv5.50.1
 # File: select.py
 # v3.6.0-c1: Added house state override and zone presence mode selects
 #
